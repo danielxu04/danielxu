4be188732d6390c08617a4cd73236ae93095a5a7
Daniel Xu <danielxu004@gmail.com>
2022-11-01 11:39:50 -0400

    fixed logo image display issue
    changed the folder location of images, and used next.js Image to display personal logo

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,13 +1,20 @@
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 import NavButton from "./NavButton";
 
-import logo from "../images";
+import Image from 'next/image';
+
+import Logo from '../../public/images/daniel-xu-logo.jpg';
 
 function NavBar(){
+
     return (
         <nav className="bg-zinc-900 border-indigo-200 borderx-indigo-500 px-3 py-3">
             <div className="container mx-auto justify-between flex flex-wrap items-center">
 
-                <img src={logo} alt="daniel-xu-logo" className="h-10 w-10" />
+                <div className="w-20">
+                    <Image src={Logo} alt="daniel-xu-logo"/>
+                </div>
+
 
                 <div className="w-full">
                     <ul className="flex flex-col rounded bg-zinc-900 mt-2 mb-1">
--- pages/components/NavButton.js
+++ pages/components/NavButton.js
@@ -2,7 +2,7 @@
 
 function NavButton(props){
     return(
-        <a href={props.id} className="block text-py-2 pr-4 pl-3 text-white rounded hover:bg-violet-500">{props.text}</a>
+        <a href={props.id} className="block text-py-2 pr-4 pl-3 text-violet-600 rounded hover:text-violet-500">{props.text}</a>
     );
 }
 
