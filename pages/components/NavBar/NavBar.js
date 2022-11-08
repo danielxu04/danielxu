367e3304aa777a2a75c7e4fb1e98a9fadb099acf
Daniel Xu <danielxu004@gmail.com>
2022-11-08 13:57:49 -0500

    hamburger dropdown setup, responsive navbar implementation
    updated logo image, added hamburger dropdown menu, displayed through an svg.
    at breakpoint 768px, conceals NavButtons

--- pages/components/NavBar/NavBar.js
+++ pages/components/NavBar/NavBar.js
@@ -1,23 +1,25 @@
 import NavButton from "./NavButton";
 import Image from "next/image";
 
-import codeLogo from "../../../public/icons/code.svg";
+import codeLogo from "../../../public/icons/code-solid.svg";
+import hamburger from "../../../public/icons/bars-solid.svg";
 
 function NavBar(){
 
     return (
-        <nav className="bg-black px-3 py-3">
-            <div className="container mx-auto justify-between flex flex-wrap items-center">
+        <nav className="sm:w-screen bg-black px-3 py-3">
+            <div className="sm:w-screen md:container mx-auto justify-between flex flex-wrap items-center">
 
-                <div className="w-20">
-                    <div className="inline">
-                        <h1 className="font-robotoMonoBold text-violet-500 text-2xl">DX</h1>
-                        <Image src={codeLogo} alt="code-logo" className="filter-violet-500"/>
+                <div>
+                    <div className="sm:pl-4">
+                        <h1 className="font-robotoMonoBold text-violet-500 text-2xl inline-block">DX</h1>
+                        <Image src={codeLogo} alt="code-logo" className="filter-violet-500 inline-block ml-2 pb-2 w-7"/>
                     </div>
                 </div>
-
-
-                <div >
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-violet-500 rounded-lg md:hidden hover:bg-violet-300 focus:outline-none focus:ring focus:ring-violet-500" aria-controls="navbar-default" aria-expanded="false">
+                    <Image src={hamburger} alt="navbar-hamburger" className="filter-violet-600 w-6"/>
+                </button>
+                <div className="hidden md:block" id="navbar-default">
                     <ul className="flex flex-row rounded p-4 bg-black mt-2 mb-1">
                         <li>
                             <NavButton text="Home" id="#home"/>
