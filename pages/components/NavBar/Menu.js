d69603d8896ce5230c5417e40a4e24dad85e50a4
Daniel Xu <danielxu004@gmail.com>
2022-11-09 00:12:20 -0500

    added transition property to menu

--- pages/components/NavBar/Menu.js
+++ pages/components/NavBar/Menu.js
@@ -2,7 +2,7 @@ import NavButton from "./NavButton";
 
 function Menu(props){
     return (
-        <div className={`flex bg-zinc-800 h-screen top-0 flex-col absolute z-30 pt-72 text-right left-0 w-screen ${props.click ? 'translate-x-0' : 'translate-x-full'}`}>
+        <div className={`flex bg-zinc-800 h-screen top-0 flex-col absolute z-30 pt-72 text-right left-0 w-screen transition ${props.click ? 'translate-x-0' : 'translate-x-full'}`}>
             <div className="pt-4 pb-4"><NavButton text="Home" id="#home"/></div>
             <div className="pt-4 pb-4"><NavButton text="Projects" id="#projects"/></div>
             <div className="pt-4 pb-4"><NavButton text="Contact" id="#contact"/></div>
