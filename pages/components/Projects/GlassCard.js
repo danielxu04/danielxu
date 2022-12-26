18a77263b3b8e5ea892d34a9c89a466d7322db66
Daniel Xu <danielxu004@gmail.com>
2022-12-26 13:35:16 -0500

    changed glasscard h3

--- pages/components/Projects/GlassCard.js
+++ pages/components/Projects/GlassCard.js
@@ -14,7 +14,7 @@ function GlassCard(props){
             <div className="glass-card">
                 <div className="glass-content text-white">
                     <h2 className="font-montserratBold">01</h2>
-                    <h3 className="font-montserratRegular">Card One</h3>
+                    <h3 className="font-montserratRegular">Card Title</h3>
                     <p className="font-montserratRegular">Lorem ipsum dolor sit amet, consecteteur adipisicing elit, sed do
                         eiusmod tempor...
                     </p>
