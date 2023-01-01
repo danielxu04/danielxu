d551b708258f6eec1381161af52039a2cd61026c
Daniel Xu <danielxu004@gmail.com>
2023-01-01 02:42:14 -0500

    added scroll animation and hover animation on glass cards

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
-// our-domain.com/
+import {useEffect} from 'react';
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 import HeaderSection from './components/HeaderSection';
 import AboutMe from './components/AboutMe/AboutMe';
 import ProjectSection from './components/Projects/ProjectSection';
 
 function HomePage() {
+    useEffect(() => {
+        Aos.init({duration: 1000});
+    }, []);
     return (
         <div className="flex flex-col w-screen">
                 <HeaderSection />
-                <AboutMe />
-                <ProjectSection />
+                <div data-aos="fade-up"><AboutMe /></div>
+                <div data-aos="fade-up"><ProjectSection /></div>
         </div>
     );
 }
