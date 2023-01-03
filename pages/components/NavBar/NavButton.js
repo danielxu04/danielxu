e7f84ed256d37ce50332a794876213891ff910fa
Daniel Xu <danielxu004@gmail.com>
2023-01-03 01:49:36 -0500

    added new hover effects on nav items

--- pages/components/NavBar/NavButton.js
+++ pages/components/NavBar/NavButton.js
@@ -1,6 +1,6 @@
 function NavButton(props){
     return(
-        <a href={props.id} className="block text-py-2 px-5 lg:px-7 font-ubuntuMedium text-purple-800 purple-glow rounded text-lg hover:text-violet-500 hover:scale-110 hover:translate-y-1">{props.text}</a>
+        <a href={props.id} className="nav-items text-py-2 px-5 lg:px-7 font-ubuntuMedium text-purple-800 purple-glow rounded text-lg hover:scale-110">{props.text}</a>
     );
 }
 
