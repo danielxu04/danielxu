6d25a92902204f9426d2319b06b45cc0c3d38c4a
Daniel Xu <danielxu004@gmail.com>
2023-01-03 03:33:37 -0500

    added favicon and website title

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document(){
     return (
         <Html className="m-0 overflow-x-hidden scroll-smooth p-0">
-                <Head />
+                <Head>
+                        <title>Daniel Xu</title>
+                        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                </Head>
                 <body className="transition-300 relative m-0 min-h-screen overflow-hidden overflow-x-hidden bg-black p-0">
                 <Main />
                 <NextScript />
