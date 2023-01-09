8fcef0ef6df56dc25afffbb23b56d4e4a3ca4959
Daniel Xu <danielxu004@gmail.com>
2023-01-09 18:10:57 -0500

    centered footer section

--- pages/components/Footer/FooterIcons.js
+++ pages/components/Footer/FooterIcons.js
@@ -5,7 +5,7 @@ import Instagram from '../../../public/icons/square-instagram.svg';
 
 function FooterIcons(){
     return (
-        <ul className="flex flex-row rounded xl:pr-[34vw]">
+        <ul className="flex flex-row rounded">
             <li className="px-4 xl:px-1">
                 <a href="https://www.linkedin.com/in/daniel-xu-35a069241/" className="icon-box-shadow">
                     <Image src={LinkedIn} alt="linkedin" className="filter-purple-800 opacity-70 inline-block ml-2 mb-2 w-6 2xs:w-7 hover:translate-y-[-0.25vh] hover:opacity-100 footer-icon"/>
--- pages/components/Footer/FooterSection.js
+++ pages/components/Footer/FooterSection.js
@@ -3,13 +3,13 @@ import FooterIcons from './FooterIcons';
 function FooterSection(){
     return (
         <div className="bg-transparent pt-10 2xs:pt-3 py-3 flex flex-col xl:block">
-            <div className="flex justify-center pt-2 xl:pl-[1.5vw] xl:hidden xl:text-left">
+            <div className="flex justify-center pt-2 xl:hidden">
                 <FooterIcons />
             </div>
-            <div className="flex justify-center xl:inline-block xl:text-right">
-                <p className="text-purple-600 text-[0.65rem] 2xs:text-sm font-montserratRegular lighter-purple-glow xl:pl-[23vw] 2xl:pl-[27vw] xl:text-md">Created and designed by Daniel Xu, 2023.</p>
+            <div className="flex justify-center xl:inline-block xl:text-right xl:w-[55%] 2xl:w-[52.5%]">
+                <p className="text-purple-600 text-[0.65rem] 2xs:text-sm font-montserratRegular lighter-purple-glow xl:pl-[23vw]">Created and designed by Daniel Xu, 2023.</p>
             </div>
-            <div className="hidden justify-center pt-2 xl:pl-[1.5vw] xl:inline-block xl:text-left">
+            <div className="hidden justify-center pt-2 xl:pl-[1.5vw] xl:inline-block xl:text-left xl:w-[45%] 2xl:w-[47.5%]">
                 <FooterIcons />
             </div>
         </div>
