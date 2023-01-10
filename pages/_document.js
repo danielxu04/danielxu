e92e8f4da59bf1568a16080f2b79e1a5af66e66a
Daniel Xu <danielxu004@gmail.com>
2023-01-09 20:08:59 -0500

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,10 @@ export default function Document(){
         <Html className="m-0 overflow-x-hidden scroll-smooth p-0">
                 <title>Daniel Xu</title>
                 <Head>
-                    <link rel="icon" href="/favicon/favicon.ico" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
                 </Head>
                 <body className="transition-300 relative m-0 min-h-screen overflow-hidden overflow-x-hidden bg-black p-0">
                 <Main />
