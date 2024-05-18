a19cb58266a9f70371422ef77bc341e78f8d1464
Daniel Xu <danielxu004@gmail.com>
2024-05-18 18:35:45 -0400

    Update AboutMe

--- components/AboutMe/IntroParagraph.js
+++ components/AboutMe/IntroParagraph.js
@@ -16,10 +16,14 @@ function IntroParagraph(){
             <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}>
                 Combinatorics & Optimization, </strong>and
             <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> Pure Mathematics.</strong>
-            <br /><br />In my current school term, I am taking 6 courses: Intro to Optimization (Advanced), Statistics (Advanced), Foundations of Sequential Programs, Computer Organization and Design, Physics I, and Korean!
+            <div className="hidden">In my current school term, I am taking 6 courses: Intro to Optimization (Advanced), Statistics (Advanced), Foundations of Sequential Programs, Computer Organization and Design, Physics I, and Korean!</div>
             <br /><br />I&apos;m super passionate about computer science and theoretical mathematics, in various areas such as Quantitative Finance, Full-stack Development, Data Science, Deep Learning, and Computer Vision.
 
-            <br /><br />Formerly, I was a Full-stack Developer at 
+            <br /><br />Currently, I am working as a 
+            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> Quant Analyst - Derivatives, Collateral, Fixed Income </strong>
+             at 
+            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-green-300 purple-glow' : 'text-green-600 blackberry-glow'}`}> HOOPP</strong>
+            . Formerly, I was a Full-stack Developer at 
             <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-blue-300 purple-glow' : 'text-blue-600 blackberry-glow'}`}> BlackBerry,</strong> in charge of leveraging a versatile tech stack,
             consisting of 
             <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> AEM,</strong>
@@ -28,15 +32,6 @@ function IntroParagraph(){
             <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> Java,</strong>
             <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> Git,</strong> and
             <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> XML/HTL/SASS</strong>.
-            <br /><br />Most notably, I self-implemented a next-gen image converter using 
-            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> Python</strong> in conjunction with 
-            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> BeautifulSoup</strong>, 
-            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> Pillow</strong> and 
-            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> customtkinter</strong>, to automate the conversion of older image formats (JPG, PNG) to WebP format. This reduced bundle size, 
-            which 
-            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> saved countless hours</strong> for current/future developers, 
-            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> thousands of dollars</strong> for BlackBerry, and boosted Lighthouse performance metrics by up to 
-            <strong className={`transition-15 ${lightModeState.isLightMode === 'light' ? 'text-purple-300 purple-glow' : 'text-fuchsia-700 light-purple-glow'}`}> 135.7%</strong>.
 
             <br /><br />When I&apos;m not coding, you&apos;ll find me playing piano, playing basketball, watching K-Dramas and Anime, or hanging out with friends.
 
